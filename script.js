280643ccd7d91b03612c9c5a172222c0c41d6c54
JJSS25 <138161004+JJSS25@users.noreply.github.com>
2026-04-28 10:45:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -203,17 +203,3 @@ crearSalir();
 
 /* INICIO */
 actualizarHotspots("pasillo1");
-
-const scene = document.querySelector('a-scene');
-
-scene.addEventListener('loaded', () => {
-    const loader = document.getElementById('loader');
-
-    if(loader){
-        loader.style.opacity = "0";
-
-        setTimeout(() => {
-            loader.style.display = "none";
-        }, 500);
-    }
-});
